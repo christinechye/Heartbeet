deec776a6f09c8f9fe9c78f2705205ec8faee1c7
Liying Liang <liyingliang@Liyings-MacBook-Pro.local>
2021-02-21 01:39:07 -0800

    authenticate

--- app/src/main/java/com/example/livewell/MainActivity.java
+++ app/src/main/java/com/example/livewell/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.livewell;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,8 +24,11 @@ public class MainActivity extends AppCompatActivity {
     public final static String USERNAME = "Christine@gmail.com";
     public final static String PASSWORD = "123456";
 
+    private FirebaseAuth fAuth;
+
     EditText username;
     EditText password;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +41,46 @@ public class MainActivity extends AppCompatActivity {
         // initialize UI elements
         username = findViewById(R.id.et_username);
         password = findViewById(R.id.et_password);
+
+        loginButton = findViewById(R.id.btn_login);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String email = username.getText().toString().trim();
+                String pwd = password.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    username.setError("Email is required.");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(pwd)) {
+                    password.setError("Password is required.");
+                    return;
+                }
+
+                fAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        String usernameInput = username.getText().toString();
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Logged In Successfully!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                            int atIndex = usernameInput.indexOf("@");
+                            String username = usernameInput.substring(0, atIndex);
+                            intent.putExtra("name", username);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(MainActivity.this, "Error! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
     }
 
     //ToDo: 1. Implement the callback methods
@@ -64,26 +115,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //ToDo: 2. Implement click event handler method
-    public void login(View view) {
-        // input
-        String usernameInput = username.getText().toString();
-        String passwordInput = password.getText().toString();
-
-        // validate username
-        if (usernameInput.compareTo(USERNAME) == 0 && passwordInput.compareTo(PASSWORD) == 0) {
-            Intent intent = new Intent(this, SecondActivity.class);
-            String username = usernameInput.substring(0, 9);
-            intent.putExtra("name", username);
-            startActivity(intent);
-        } else { // otherwise: toast message
-            Context context = getApplicationContext();
-            CharSequence text = "Invalid Username or Password.";
-            int duration = Toast.LENGTH_SHORT;
-
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-        }
-    }
+//    public void login(View view) {
+//        // input
+//        String usernameInput = username.getText().toString();
+//        String passwordInput = password.getText().toString();
+//
+//        // validate username
+//        if (usernameInput.compareTo(USERNAME) == 0 && passwordInput.compareTo(PASSWORD) == 0) {
+//            Intent intent = new Intent(this, SecondActivity.class);
+//            String username = usernameInput.substring(0, 9);
+//            intent.putExtra("name", username);
+//            startActivity(intent);
+//        } else { // otherwise: toast message
+//            Context context = getApplicationContext();
+//            CharSequence text = "Invalid Username or Password.";
+//            int duration = Toast.LENGTH_SHORT;
+//
+//            Toast toast = Toast.makeText(context, text, duration);
+//            toast.show();
+//        }
+//    }
 
     public void signup(View view) {
         Intent intent = new Intent(this, ThirdActivity.class);
--- app/src/main/java/com/example/livewell/ThirdActivity.java
+++ app/src/main/java/com/example/livewell/ThirdActivity.java
@@ -3,20 +3,31 @@ package com.example.livewell;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ThirdActivity extends AppCompatActivity {
     public final static String TAG = "ThirdActivity";
 
     public final static String USERNAME = "Christine@gmail.com";
 
+    private FirebaseAuth fAuth;
+
     EditText username;
     EditText password;
+    Button signupButton;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +39,44 @@ public class ThirdActivity extends AppCompatActivity {
         // get and set password and username
         username = findViewById(R.id.et_username1);
         password = findViewById(R.id.et_password1);
+
+        signupButton = findViewById(R.id.btn_signup);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        signupButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String email = username.getText().toString().trim();
+                String pwd = password.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    username.setError("Email is required.");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(pwd)) {
+                    password.setError("Password is required.");
+                    return;
+                }
+
+//                if (fAuth.getCurrentUser() != null) {
+//                    Intent intent = new Intent(ThirdActivity.this, SecondActivity.class);
+//                    startActivity(intent);
+//                }
+
+                fAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(ThirdActivity.this, "User created.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(ThirdActivity.this, "Error! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 
     //ToDo: 1. Implement the callback methods
@@ -61,18 +110,18 @@ public class ThirdActivity extends AppCompatActivity {
         Log.i(TAG, "On Destroy");
     }
 
-    public void signup(View view) {
-        String usernameInput = username.getText().toString();
-
-        if (usernameInput.compareTo(USERNAME) == 0) {
-            Context context = getApplicationContext();
-            CharSequence text = "You already have an account with this username.";
-            int duration = Toast.LENGTH_SHORT;
-
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-        }
-    }
+//    public void signup(View view) {
+//        String usernameInput = username.getText().toString();
+//
+//        if (usernameInput.compareTo(USERNAME) == 0) {
+//            Context context = getApplicationContext();
+//            CharSequence text = "You already have an account with this username.";
+//            int duration = Toast.LENGTH_SHORT;
+//
+//            Toast toast = Toast.makeText(context, text, duration);
+//            toast.show();
+//        }
+//    }
 
     //ToDo: 3. Implement click event handler method
     public void login(View view) {
